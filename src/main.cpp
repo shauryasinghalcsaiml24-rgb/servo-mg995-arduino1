72c913d2f974cad954014817a697982fe58b994f
shauryasinghalcsaiml24-rgb <shauryasinghal150@gmail.com>
2026-02-04 14:09:17 +0530

    initialize pin

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
 #include <Arduino.h>
+#include <Servo.h>
+Servo myServo;
+
 void setup() {
-// write your initialization code here
+    Serial.begin(9600);
+    myServo.attach(11);
+    Serial.println("Servo initiate");
+    // write your initialization code here
 }
-// write your code here
+
+void loop() {
+    for (int i =0; i <= 180; i += 10) {
+        myServo.write(i);
+        Serial.print("Servo Angle: ");
+        Serial.println(i);
+        delay(500);
+    }
+    for (int i =180; i >= 0; i -= 10) {
+        myServo.write(i);
+        Serial.print("Servo Angle: ");
+        Serial.println(i);
+        delay(500);
+    }
+
+    // write your code here
 }
